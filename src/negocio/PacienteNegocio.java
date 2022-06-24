9e590410b21bd36d4843ff573067c74b5755881b
Hest10 <hermest10@aol.com>
2022-06-24 18:04:08 -0300

    PacienteNegocio y PacienteNegocioImpl

--- src/negocio/PacienteNegocio.java
+++ src/negocio/PacienteNegocio.java
@@ -1,5 +1,15 @@
 package negocio;
 
+import entidad.Paciente;
+
 public interface PacienteNegocio {
+
+	public boolean agregarPaciente(Paciente agregar);
+
+	public boolean modificarPaciente(Paciente modificar);
+
+	public Paciente buscar_paciente(int idPaciente);
+
+	public boolean bajaPaciente(Paciente baja);
 
 }
--- src/negocioImpl/PacienteNegocioImpl.java
+++ src/negocioImpl/PacienteNegocioImpl.java
@@ -1,7 +1,34 @@
 package negocioImpl;
 
+import dao.PacienteDao;
+import daoImpl.PacienteDaoImpl;
+import entidad.Paciente;
 import negocio.PacienteNegocio;
 
 public class PacienteNegocioImpl implements PacienteNegocio{
 
+	@Override
+	public boolean agregarPaciente(Paciente agregar) {
+		PacienteDao padao = new PacienteDaoImpl();
+		return padao.Agregar(agregar);
+	}
+
+	@Override
+	public boolean modificarPaciente(Paciente modificar) {
+		PacienteDao padao = new PacienteDaoImpl();
+		return padao.Modificar(modificar);
+	}
+
+	@Override
+	public Paciente buscar_paciente(int idPaciente) {
+		PacienteDao padao = new PacienteDaoImpl();
+		return padao.Buscar(idPaciente);
+	}
+
+	@Override
+	public boolean bajaPaciente(Paciente baja) {
+		PacienteDao padao = new PacienteDaoImpl();
+		padao.Eliminar(baja.getIdPaciente());
+		return false;
+	}
 }
