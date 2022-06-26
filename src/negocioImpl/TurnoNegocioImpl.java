1da93a848bdbf1097370e79455c78daaf7040371
Hest10 <hermest10@aol.com>
2022-06-26 20:40:56 -0300

    TurnoNegocio y TurnoNegocioImpl

--- src/negocioImpl/TurnoNegocioImpl.java
+++ src/negocioImpl/TurnoNegocioImpl.java
@@ -1,7 +1,38 @@
 package negocioImpl;
 
+import dao.TurnoDao;
+import daoImpl.TurnoDaoImpl;
+import entidad.Turno;
 import negocio.TurnoNegocio;
 
 public class TurnoNegocioImpl implements TurnoNegocio{
 
+	@Override
+	public boolean agregarTurno(Turno agregar) {
+		TurnoDao tdao = new TurnoDaoImpl();
+		return tdao.Agregar(agregar);
+	}
+	
+	@Override
+	public boolean Modificar(Turno modificar) {
+		TurnoDao tdao = new TurnoDaoImpl();
+		return tdao.Modificar(modificar);
+	}
+	
+	
+	@Override
+	public boolean Eliminar(int baja) {
+		TurnoDao tdao = new TurnoDaoImpl();
+		return tdao.Eliminar(baja);
+	}
+
+	@Override
+	public Turno Buscar(int idTurno) {
+		TurnoDao tdao = new TurnoDaoImpl();
+		return tdao.Buscar(idTurno);
+	}
+
+	
+
+	
 }
